52cc26f9172150cbb669e4e18fb8b7736b6985f6
aelmardhi <aelmardhi@gmail.com>
2022-09-29 12:04:56 +0200

    initial

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+loadConfigs(){
+    const Configs = [];
+    
+
+} 
+const Configs = loadConfigs()
+
+
+switch (process.argv[2]) { //switch Command
+    case 'set':
+        
+        break;
+
+    default:
+        break;
+}
+
