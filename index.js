589f42e36eaf8041fcadec0ed9ae6a3b84d332d2
aelmardhi <aelmardhi@gmail.com>
2022-09-29 15:25:04 +0200

    feat : include

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
 const Store =  require('./lib/store');
 
-const store = new Store()
+(async function main(){
+    const store =   new Store()
+    await store.loadConfigs()
+    console.log(store.config);
 
 
+    switch (process.argv[2]) { //switch Command
+        case 'include':
+            store.include (process.argv[3]);
+            break;
 
-switch (process.argv[2]) { //switch Command
-    case 'include':
-        store.include (process.argv[3]);
-        break;
-
-    default:
-        break;
-}
-
+        default:
+            break;
+    }
+})()
