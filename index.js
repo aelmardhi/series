667960db7dc91eabc7dc484def680aee0cbc919e
aelmardhi <aelmardhi@gmail.com>
2022-10-01 09:29:11 +0200

    feat : add command shortcut

--- index.js
+++ index.js
@@ -8,15 +8,19 @@ const Store =  require('./lib/store');
 
     switch (process.argv[2]) { //switch Command
         case 'include':
+        case 'i':
             store.include (process.argv[3]);
             break;
         case 'set':
+        case 's':
             store.set(process.argv[3], process.argv[4]);
             break;
         case 'current':
+        case 'c':
             store.current(process.argv[3]);
             break;
         case 'next':
+        case 'n':
             store.next(process.argv[3]);
             break;
 
