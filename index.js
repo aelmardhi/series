3b92f2016611666bc8f7b5fd91abcc3cc0fe303c
aelmardhi <aelmardhi@gmail.com>
2022-10-15 00:41:38 +0200

    feat : missing argument

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { argv } = require('process');
 const Store =  require('./lib/store');
 
 (async function main(){
@@ -9,22 +10,46 @@ const Store =  require('./lib/store');
     switch (process.argv[2]) { //switch Command
         case 'include':
         case 'i':
-            store.include (process.argv[3]);
+            if(instruction(['include', 'i'], ['<PATH>'], [process.argv[3]]))
+                store.include (process.argv[3]);
             break;
         case 'set':
         case 's':
-            store.set(process.argv[3], process.argv[4]);
+            if(instruction(['set', 's'], ['<NAME>','<PATH>'], [process.argv[3], process.argv[4]]))
+                store.set(process.argv[3], process.argv[4]);
             break;
         case 'current':
         case 'c':
-            store.current(process.argv[3]);
+            if(instruction(['current', 'c'], ['<NAME>'], [process.argv[3]]))
+                store.current(process.argv[3]);
             break;
         case 'next':
         case 'n':
-            store.next(process.argv[3]);
+            if(instruction(['next', 'n'], ['<NAME>'], [process.argv[3]]))
+                store.next(process.argv[3]);
             break;
 
         default:
             break;
     }
 })()
+
+
+function instruction( commands, args, values){
+    let count = 0;
+    for( v of values){
+        if(v)
+            count++;
+    }
+    if(count >= values.length)
+        return true;
+    console.log('Missing arguments, the command should be in the form:');
+    for(c of commands){
+        let i = '   series '+c;
+        for(let j=0; j< args.length;j++){
+            i += ' '+ args[j];
+        }
+        console.log(i);
+    }
+    return false;
+}
